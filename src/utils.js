6c9f1bc7b58953b6b87fcec875558b7dd7c1f5fb
Warren Lu <warren.lu6@gmail.com>
2025-06-09 14:31:06 -0400

    deep compare

--- src/utils.js
+++ src/utils.js
@@ -149,6 +149,29 @@ export function deepCopy(input) {
 
   return output;
 };
+/**
+ * Deeply compare two objects for equality.
+ * @param {object} obj1 
+ * @param {object} obj2 
+ * @returns boolean
+ */
+export function deepCompare(obj1, obj2) {
+  if (obj1 === obj2) return true;
+
+  if (typeOf(obj1) !== typeOf(obj2) || typeOf(obj1) !== 'object') return false;
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (const key of keys1) {
+    if (!keys2.includes(key)) return false;
+    if (!deepCompare(obj1[key], obj2[key])) return false;
+  }
+
+  return true;
+}
 
 /**
  * Further separate 'null' type from 'object' type.
